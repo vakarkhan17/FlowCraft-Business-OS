965ba8a1090d898082a9d809c60a2f7303291c5a
Vakar Khan <vakarkhan@gmail.com>
2026-07-14 22:16:16 +0400

    feat(database): add enterprise master data schema

--- apps/api/src/digital-dna/digital-dna.service.ts
+++ apps/api/src/digital-dna/digital-dna.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 
-export type DnaObject = 'TEN' | 'CMP' | 'BRN' | 'USR' | 'CUR' | 'GRP' | 'LEN' | 'PLT' | 'BUN' | 'DIV' | 'DEP' | 'SEC' | 'TEM' | 'LOC' | 'CCT' | 'PCT' | 'ORG';
+export type DnaObject = 'TEN' | 'CMP' | 'BRN' | 'USR' | 'CUR' | 'GRP' | 'LEN' | 'PLT' | 'BUN' | 'DIV' | 'DEP' | 'SEC' | 'TEM' | 'LOC' | 'CCT' | 'PCT' | 'ORG' | 'CTY' | 'TER' | 'CON' | 'UOM' | 'IGR' | 'ICA' | 'BRD' | 'MFR' | 'ITM' | 'WHS' | 'WHZ' | 'BIN' | 'BPR';
 
 @Injectable()
 export class DigitalDnaService {
